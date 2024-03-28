559dc0011ce9adc341e53ad80baad011f45d79de
saurabh gawade <saurabhgawade@finacus.co.in>
2024-03-28 17:23:55 +0530

    UPDATE

--- src/components/Dashview.js
+++ src/components/Dashview.js
@@ -1,5 +1,10 @@
 import React from "react";
 import cash from "../Asset/icon1-removebg-preview.png";
+import cash2 from "../Asset/Icon2-removebg-preview.png";
+import cash3 from "../Asset/Icon3-removebg-preview.png";
+import cash4 from "../Asset/Icon4-removebg-preview.png";
+import cash5 from "../Asset/icon5-removebg-preview.png";
+import cash6 from "../Asset/icon6-removebg-preview.png";
 import "../style/style.css";
 
 const Dashview = () => {
@@ -53,10 +58,10 @@ const Dashview = () => {
                   <div className="card-body">
                     <div className="row">
                       <div className="col-5">
-                        <img src={cash} alt="cash" className="img-fluid" />
+                        <img src={cash2} alt="cash" className="img-fluid" />
                       </div>
                       <div className="col-7">
-                        <small className="text-secondary">Earning</small>
+                        <small className="text-secondary">Orders</small>
                         <h4 className="text-dark">$198k</h4>
                         <small>
                           <span className="price">37.8%</span>
@@ -72,10 +77,10 @@ const Dashview = () => {
                   <div className="card-body">
                     <div className="row">
                       <div className="col-5">
-                        <img src={cash} alt="cash" className="img-fluid" />
+                        <img src={cash3} alt="cash" className="img-fluid" />
                       </div>
                       <div className="col-7">
-                        <small className="text-secondary">Earning</small>
+                        <small className="text-secondary">Balance</small>
                         <h4 className="text-dark">$198k</h4>
                         <small>
                           <span className="price">37.8%</span>
@@ -91,10 +96,10 @@ const Dashview = () => {
                   <div className="card-body">
                     <div className="row">
                       <div className="col-5">
-                        <img src={cash} alt="cash" className="img-fluid" />
+                        <img src={cash4} alt="cash" className="img-fluid" />
                       </div>
                       <div className="col-7">
-                        <small className="text-secondary">Earning</small>
+                        <small className="text-secondary">Total Sales</small>
                         <h4 className="text-dark">$198k</h4>
                         <small>
                           <span className="price">37.8%</span>
@@ -115,7 +120,7 @@ const Dashview = () => {
                     <div className="overviewTitleAndDropdown">
                       <div className="wrapTitleAndDrop d-flex justify-content-between">
                         <h5 className="fw-bold">Overview</h5>
-                        <select name="" id="">
+                        <select name="" id="" className="border-0">
                           <option value="">Quaterly</option>
                         </select>
                       </div>
@@ -276,6 +281,150 @@ const Dashview = () => {
                       </div>
                     </div>
                   </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="wrap-table my-4">
+            <div className="card">
+              <div className="card-body">
+                <div className="first-section d-flex justify-content-between">
+                  <h4>Product Sell</h4>
+                  <div className="right-section d-flex">
+                    <div className="wrap-search me-2">
+                      <div className="input-group">
+                        <span
+                          className="input-group-text bg-white border-0"
+                          id="inputGroup-sizing-sm"
+                        >
+                          🔎
+                        </span>
+                        <input
+                          placeholder="Search"
+                          type="text"
+                          className="form-control searchIcon ps-0 border-0"
+                          aria-label="Sizing example input"
+                          aria-describedby="inputGroup-sizing-sm"
+                        />
+                      </div>
+                    </div>
+                    <select name="" id="" className="border-0">
+                      <option value="">last 30 days</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="actual-table">
+                  <table class="table table-hover">
+                    <thead>
+                      <tr>
+                        <th scope="col">
+                          <small className="text-secondary">Product Name</small>
+                        </th>
+                        <th scope="col">
+                          <small className="text-secondary">Stack</small>
+                        </th>
+                        <th scope="col">
+                          <small className="text-secondary">Price</small>
+                        </th>
+                        <th scope="col">
+                          <small className="text-secondary">Total Sales</small>
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th scope="row">
+                          <div className="wrap-image-content d-flex">
+                            <img src={cash5} alt="cash5" />
+                            <div className="wrap-only-content ms-2">
+                              <h5 className="contentTitle mb-0">Abstract 3D</h5>
+                              <small className="text-secondary fw-normal">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Earum, aliquid!
+                              </small>
+                            </div>
+                          </div>
+                        </th>
+                        <td className="">
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            32in Stock
+                          </p>
+                        </td>
+                        <td>
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            $45.99
+                          </p>
+                        </td>
+                        <td>
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            20
+                          </p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">
+                          <div className="wrap-image-content d-flex">
+                            <img src={cash6} alt="cash5" />
+                            <div className="wrap-only-content ms-2">
+                              <h5 className="contentTitle mb-0">
+                                Illustrator Adobe
+                              </h5>
+                              <small className="text-secondary fw-normal">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Earum, aliquid!
+                              </small>
+                            </div>
+                          </div>
+                        </th>
+                        <td className="">
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            32in Stock
+                          </p>
+                        </td>
+                        <td>
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            $45.99
+                          </p>
+                        </td>
+                        <td>
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            20
+                          </p>
+                        </td>
+                      </tr>
+                      <tr>
+                        <th scope="row">
+                          <div className="wrap-image-content d-flex">
+                            <img src={cash5} alt="cash5" />
+                            <div className="wrap-only-content ms-2">
+                              <h5 className="contentTitle mb-0">
+                                Maya Max 3Dx
+                              </h5>
+                              <small className="text-secondary fw-normal">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Earum, aliquid!
+                              </small>
+                            </div>
+                          </div>
+                        </th>
+                        <td className="">
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            32in Stock
+                          </p>
+                        </td>
+                        <td>
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            $45.99
+                          </p>
+                        </td>
+                        <td>
+                          <p className="d-flex align-items-center mt-0 table-text">
+                            20
+                          </p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
